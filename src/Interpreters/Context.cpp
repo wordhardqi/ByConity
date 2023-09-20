f9b41248e2d109ec905a731d83becf9ed23cd8e7
rmq <rmq@bytedance.com>
2023-09-20 22:28:28 +0800

    fix use database

--- src/Interpreters/Context.cpp
+++ src/Interpreters/Context.cpp
@@ -1949,6 +1949,9 @@ void Context::setCurrentDatabase(const String & name, ContextPtr local_context)
         {
             throw Exception(ErrorCodes::BAD_ARGUMENTS, "catalog {} does not exist", catalog_name);
         }
+    } else 
+    {
+        use_cnch_catalog = true;
     }
 
     auto db_name_with_tenant_id = appendTenantIdOnly(database_opt.value());
